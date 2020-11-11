d2ba374bf8c06dff259fe68c7e66deed093f79ce
Sumi Kim <ksm2920@gmail.com>
2020-11-11 12:50:07 +0100

    save

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 window.onload = function() {
-    
     let todoForm = document.querySelector('.todo-form');
-    let filter = document.querySelector("#filter");
+    let filter = document.querySelector("#search-input");
     let todoList = document.querySelector('.todo-items');
     let clearButton = document.querySelector("#clear-button");
     
@@ -17,23 +16,22 @@ let todos = [];
 let filterText = "";
 
 function todo(e) {
-    let newTodo = document.querySelector('.todo-input');
+    let newTodo = document.querySelector('#todo-input');
     
     e.preventDefault();
     addTodo(newTodo.value); 
     
     function addTodo() {
         let item = newTodo.value;
+        
         if (item !== '') {
             let todo = {
                 id: Date.now(),
                 name: item,
                 completed: false
             };
-            
             todos.push(todo);
             addToLocalStorage(todos);
-            
             newTodo.value = '';
         }
         else {
@@ -44,56 +42,45 @@ function todo(e) {
 
 function generateHTML() {
     let todoList = document.querySelector('.todo-items');
+    
     todoList.innerHTML = '';
     
     todos.forEach(function(item) {
         let checked = item.completed ? 'checked': null;
-        
         let li = document.createElement('li');
-        
         li.setAttribute('class', 'item');
-        
         li.setAttribute('data-key', item.id);
         
         if (item.completed === true) {
             li.classList.add('checked');
         }
-        
-        li.innerHTML = `
+         li.innerHTML = `
         <input type="checkbox" class="checkbox" ${checked}>
         ${item.name}
         <button class="btn btn-danger delete-button">X</button>
         `;
-        
         todoList.appendChild(li);
     });
-    
     let allTask = document.querySelectorAll(".item");
     
     for (let task of allTask) {
         let item = task.textContent;
-        
         if (item.toLowerCase().indexOf(filterText) != -1) {
-            task.style.display = "block";
-            
+            task.style.display = "block";   
         } else {
             task.style.display = "none";
         }
-    };
-    
+    };   
 }
 
 function addToLocalStorage(todos) {
-    
     localStorage.setItem('todos', JSON.stringify(todos));
-    
     generateHTML();
 }
 
 function getFromLocalStorage() {
     let reference = localStorage.getItem('todos');
     if (reference) {
-        
         todos = JSON.parse(reference);
         generateHTML();
     }
@@ -101,24 +88,17 @@ function getFromLocalStorage() {
 
 function toggle(id) {
     todos.forEach(function(item) {
-        
         if (item.id == id) {
-            
             item.completed = !item.completed;
         }
     });
-    
     addToLocalStorage(todos);
 }
 
 function deleteTodo(id) {
-    
     todos = todos.filter(function(item) {
-        
         return item.id != id;
     });
-    
-    
     addToLocalStorage(todos);
 }
 
@@ -130,21 +110,16 @@ function clearAll() {
 }
 
 function clearTodo(e) {
-    
     if (e.target.type === 'checkbox') {
-        
         toggle(e.target.parentElement.getAttribute('data-key'));
     }
-    
     if (e.target.classList.contains('delete-button')) {
-        
         deleteTodo(e.target.parentElement.getAttribute('data-key'));
     }
 }
 
 function filterTodo(e) {
     filterText = e.target.value.toLowerCase();
-    generateHTML();
-    
+    generateHTML();  
 }
 
